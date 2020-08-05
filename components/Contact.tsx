93c2ed8d69be23321abee5058365987fc86019d0
William Diffey <50226488+williamdiffey@users.noreply.github.com>
2020-08-05 09:12:59 +0900

    minor bug fixes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,53 +1,53 @@
-import styles from "../Styles/Contact.module.scss";
-import { Fade, Slide } from "react-awesome-reveal";
+import styles from '../Styles/Contact.module.scss'
+import { Slide } from 'react-awesome-reveal'
 
 export default function ContactForm() {
   return (
     <div className={styles.formContainer}>
       <div className={styles.titleContainer}>
-        <Slide direction="left" delay={500} triggerOnce>
+        <Slide direction='left' delay={500} triggerOnce>
           <h1>
             Contact<span>:</span>
           </h1>
         </Slide>
       </div>
       <form
-        name="contact"
-        action="/success"
-        method="POST"
-        data-netlify="true"
+        name='contact'
+        action='/success'
+        method='POST'
+        data-netlify='true'
         className={styles.form}
       >
-        <input type="hidden" name="form-name" value="contact" />
+        <input type='hidden' name='form-name' value='contact' />
         <p>
           <input
-            type="text"
-            name="name"
-            id="yourname"
-            placeholder="Your Name"
+            type='text'
+            name='name'
+            id='yourname'
+            placeholder='Your Name'
           />
         </p>
         <p>
           <input
-            type="email"
-            name="email"
-            id="youremail"
-            placeholder="Your Email"
+            type='email'
+            name='email'
+            id='youremail'
+            placeholder='Your Email'
           />
         </p>
         <p>
           <textarea
-            name="message"
-            id="yourmessage"
-            placeholder="Your Message"
+            name='message'
+            id='yourmessage'
+            placeholder='Your Message'
           ></textarea>
         </p>
         <p>
-          <button id={styles.contactButton} type="submit">
+          <button id={styles.contactButton} type='submit'>
             Send
           </button>
         </p>
       </form>
     </div>
-  );
+  )
 }
